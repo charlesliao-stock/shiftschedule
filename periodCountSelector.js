0594fde135dbc66b3540cb7cbc5f95fe2b59571a
charlesliao-stock <charlesliao.tw@gmail.com>
2025-09-15 09:15:27 +0800

    periodCountSelector.js

--- periodCountSelector.js
+++ periodCountSelector.js
@@ -4,11 +4,12 @@ function renderPeriodCountSelector() {
   const select = document.getElementById("periodCountSelector");
   select.innerHTML = ""; // 清空舊項目
 
-  const counts = [1, 2];
+  const counts = [1, 2, 3, 4];
   counts.forEach(count => {
     const option = document.createElement("option");
     option.value = count;
     option.textContent = `${count} 週期`;
     select.appendChild(option);
   });
+
 }
